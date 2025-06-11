f889bfc5b8908f7afb966a0df6c8b55d9ebc86e1
TahaaZare <tahaazre.dev@gmail.com>
2025-06-11 09:28:03 +0330

    initial commit for dual date converter

---
+++ DualDateConverterServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+class DualDateConverterServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/helpers/DualDateHelper.php' => app_path('Helpers/DualDateHelper.php'),
+        ], 'dual-date-helper');
+    }
+
+    public function register(): void {}
+}
---
+++ src/helpers/DualDateHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Helpers;
+
+use Morilog\Jalali\Jalalian;
+use Morilog\Jalali\CalendarUtils;
+use Carbon\Carbon;
+use Exception;
+
+class DualDateHelper
+{
+    public static function toGregorian(string $date): string
+    {
+        $clean = str_replace(['/', '-', '.', ' '], '', $date);
+
+        if (!preg_match('/^\d{8}$/', $clean)) {
+            throw new Exception("تاریخ شمسی نامعتبر است.");
+        }
+
+        $y = substr($clean, 0, 4);
+        $m = substr($clean, 4, 2);
+        $d = substr($clean, 6, 2);
+
+        $jalali = Jalalian::fromFormat('Y-m-d', "$y-$m-$d");
+        return $jalali->toCarbon()->format('Y-m-d');
+    }
+
+    public static function toJalali(string $date): string
+    {
+        try {
+            $carbon = Carbon::parse($date);
+            $jalali = Jalalian::fromCarbon($carbon);
+            return $jalali->format('Y-m-d');
+        } catch (Exception $e) {
+            throw new Exception("تاریخ میلادی نامعتبر است.");
+        }
+    }
+}
